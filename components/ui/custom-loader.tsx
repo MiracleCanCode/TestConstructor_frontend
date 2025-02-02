4196b223b29e2cf05741477fff24670ca3a24c48
MiracleCanCode <pashadurov12@gmail.com>
2025-02-02 13:29:42 +0300

    update: ui loader

--- components/ui/custom-loader.tsx
+++ components/ui/custom-loader.tsx
@@ -2,9 +2,9 @@ import { Flex, Loader } from '@mantine/core'
 import { FC } from 'react'
 
 export const CustomLoader: FC = () => (
-    <Flex align='center' justify='center' h='80vh'>
-        <Loader color='blue' size='xl' />
-    </Flex>
+	<Flex align='center' justify='center' h='80vh'>
+		<Loader color='rgba(255, 255, 255, 1)' type='bars' size={40} />
+	</Flex>
 )
 
 CustomLoader.displayName = 'CustomLoader'
